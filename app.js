3c23b8ebe1b3af8d1ad1fbcead4294202004415e
victoriacliu <42283251+victoriacliu@users.noreply.github.com>
2024-05-23 00:23:21 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,130 @@
+/*
+    SETUP
+*/
+
+// Express
+var express = require('express');
+var app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+PORT = 3187;
+
+// Database
+var db = require('./database/db-connector');
+
+// Handlebars
+var exphbs = require('express-handlebars');
+const { query } = require('express');
+app.engine('.hbs', exphbs.engine({
+    extname: ".hbs"
+}));
+app.set('view engine', '.hbs');
+
+/*
+    ROUTES
+*/
+app.get('/', function (req, res) {
+    let query1 = "SELECT * FROM Equipment;";
+    db.pool.query(query1, function (error, rows, fields) {
+        res.render('index', { data: rows });
+    })
+});
+
+
+app.post('/add-equipment-form', function (req, res) {
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Equipment (equipmentName,specialEquipment) VALUES ('${data['input-equipmentName']}', '${data['input-specialEquipment']}')`;
+    db.pool.query(query1, function (error, rows, fields) {
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else {
+            res.redirect('/');
+        }
+    })
+})
+
+app.delete('/delete-equipment-ajax/', function (req, res, next) {
+    let data = req.body;
+    let equipmentID = parseInt(data.id);
+    let deleteBsg_Cert_People = `DELETE FROM bsg_cert_people WHERE pid = ?`;
+    let deleteEquipment = `DELETE FROM Equipment WHERE id = ?`;
+
+
+    // Run the 1st query
+    db.pool.query(deleteBsg_Cert_People, [equipmentID], function (error, rows, fields) {
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+        }
+
+        else {
+            // Run the second query
+            db.pool.query(deleteEquipment, [equipmentID], function (error, rows, fields) {
+
+                if (error) {
+                    console.log(error);
+                    res.sendStatus(400);
+                } else {
+                    res.sendStatus(204);
+                }
+            })
+        }
+    })
+});
+
+app.put('/put-equipment-ajax', function (req, res, next) {
+    let data = req.body;
+
+    let specialEquipment = parseInt(data.specialEquipment);
+    let equipmentName = parseInt(data.equipmentName);
+
+    let queryUpdateEquipment = `UPDATE Equipment SET specialEquipment = ? WHERE equipmentID = ?`;
+    let selectEquipment = `SELECT * FROM Equipment WHERE id = ?`
+
+    // Run the 1st query
+    db.pool.query(queryUpdateEquipment, [specialEquipment, equipmentName], function (error, rows, fields) {
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we run our second query and return that data so we can use it to update the people's
+        // table on the front-end
+        else {
+            // Run the second query
+            db.pool.query(selectEquipment, [specialEquipment], function (error, rows, fields) {
+
+                if (error) {
+                    console.log(error);
+                    res.sendStatus(400);
+                } else {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+/*
+    LISTENER
+*/
+app.listen(PORT, function () {
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
---
+++ public/js/delete_equipment.js
@@ -0,0 +1,40 @@
+function deleteEquipment(equipmentID) {
+    // Put our data we want to send in a javascript object
+    let data = {
+        id: equipmentID
+    };
+
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", "/delete-equipment-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 204) {
+
+            // Add the new data to the table
+            deleteRow(equipmentID);
+
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 204) {
+            console.log("There was an error with the input.")
+        }
+    }
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+}
+
+
+function deleteRow(equipmentID) {
+
+    let table = document.getElementById("people-table");
+    for (let i = 0, row; row = table.rows[i]; i++) {
+        //iterate through rows
+        //rows would be accessed using the "row" variable assigned in the for loop
+        if (table.rows[i].getAttribute("data-value") == equipmentID) {
+            table.deleteRow(i);
+            break;
+        }
+    }
+}
